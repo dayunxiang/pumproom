bc0011f3ad896cd0530c41f7ac978d51a1032c36
baopingli <l>
2018-05-22 10:04:04 +0800

    no

--- JuCheap.Core.Web/Controllers/RTDataController.cs
+++ JuCheap.Core.Web/Controllers/RTDataController.cs
@@ -96,6 +96,40 @@ namespace JuCheap.Core.Web.Controllers
 
         }
 
+        public IActionResult Staus()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GetData3(string sidx, string sord, int page, int rows)
+        {
+            //string s = sidx + " " + sord + " " + page.ToString() + " " + rows.ToString();
+
+            List<RTData> list = (List<RTData>)new RTData_BLL().GetRTData();
+
+            var jsonData = new
+            {
+
+                total = 1, // we'll implement later 。返回的是总共的页数,跟随数据前面的操作而变化
+                page = 1,//这个就不用变了，返回的是当前第几页
+                records = rows, // implement later 所有的条数，记录中所有的条数。
+                //id是当前记录的id号。
+                //rows = new[]{
+                //new {id = 13, cell = new[] {"13","2007-10-06","Client 3","1000.00","0.00","1000.00",null}},
+                //new {id = 12, cell = new[] {"12","2007-10-06","Client 3","1000.00","0.00","1000.00",null}}
+                rows = (
+                from s in list
+                select new
+                {
+                    id = s.Id,
+                    cell = new string[] {
+                    s.Id.ToString(),s.分区名称.ToString(),s.站点名称.ToString(),s.上传时间.ToString(),s.泵1运行状态.ToString(),s.泵2运行状态.ToString(),s.泵3运行状态.ToString(),s.泵4运行状态.ToString(),s.泵5运行状态.ToString(),s.泵6运行状态.ToString(),s.小泵1运行状态.ToString(),s.小泵2运行状态.ToString(),s.泵1手自动状态.ToString(),s.泵2手自动状态.ToString(),s.泵3手自动状态.ToString(),s.泵4手自动状态.ToString(),s.泵5手自动状态.ToString(),s.泵6手自动状态.ToString(),s.小泵1手自动状态.ToString(),s.小泵2手自动状态.ToString(),s.系统运行状态.ToString(),s.PLC故障状态.ToString(),s.压力报警状态.ToString(),s.水箱缺水状态.ToString(),s.变频器1状态.ToString(),s.变频器2状态.ToString(),s.变频器3状态.ToString(),s.变频器4状态.ToString(),s.阀门开关状态.ToString(),s.阀门到位状态.ToString(),s.停机报警.ToString(),s.泵房进水报警状态.ToString(),s.停电来电报警状态.ToString(),s.门禁报警状态.ToString(),s.烟感报警状态.ToString(),s.污水泵启停状态.ToString()}
+                }).ToArray()
+
+            };
+            return Json(jsonData);
+        }
 
 
 
